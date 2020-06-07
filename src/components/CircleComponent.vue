43b8a7ab6d12bb2bc878d4767e106b7c8e7ceab9
Jørgen Stamnes <jorgenstamnes@Jrgens-MacBook-Pro.local>
2020-06-07 14:28:21 +0200

    init

---
+++ src/components/CircleComponent.vue
@@ -0,0 +1,128 @@
+<template>
+  <div id="Web_1920__1">
+    <div id="Component_5__1" class="Component_5___1">
+      <div id="Component_4__1" class="Component_4___1">
+        <div id="Circle_component" class="Circle_component">
+          <svg class="Ellipse_1">
+            <ellipse id="Ellipse_1" rx="23.5" ry="20" cx="23.5" cy="20" />
+          </svg>
+        </div>
+        <div id="yo">
+          <span>yo</span>
+        </div>
+      </div>
+      <svg class="Path_1" viewBox="9.811 6.935 52.882 46.002">
+        <path
+          id="Path_1"
+          d="M 10.26376914978027 12.8173303604126 C 22.25614547729492 24.27511405944824 11.56497383117676 10.59415817260742 21.77379035949707 23.67132186889648 C 31.98260688781738 36.74848556518555 44.27552032470703 30.59863662719727 31.0111141204834 36.74848556518555 C 17.74670791625977 42.89833450317383 28.76758766174316 48.70354843139648 18.32607460021973 44.51975250244141 C 7.884561061859131 40.33595657348633 8.746829986572266 25.35311698913574 11.99169731140137 23.67132186889648 C 15.23656463623047 21.98952674865723 38.37940216064453 6.93477725982666 38.37940216064453 6.93477725982666 L 59.35062408447266 9.663370132446289 L 59.35062408447266 24.47728157043457 L 62.69318389892578 33.64065933227539 L 59.35062408447266 43.32098770141602 L 45.9484977722168 39.31832885742188 L 49.58481216430664 20.11370468139648 L 42.5380973815918 15.98282337188721 L 36.23017501831055 25.72217559814453 L 37.625 39.31832885742188 L 39.64329147338867 45.61336135864258 L 44.99600219726562 52.93687057495117"
+        />
+      </svg>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.mediaViewInfo {
+  --web-view-name: Web 1920 – 1;
+  --web-view-id: Web_1920__1;
+  --web-scale-on-resize: true;
+  --web-enable-deep-linking: true;
+}
+:root {
+  --web-view-ids: Web_1920__1;
+}
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  border: none;
+}
+#Web_1920__1 {
+  position: absolute;
+  width: 78px;
+  height: 57px;
+  background-color: rgba(255, 255, 255, 1);
+  overflow: hidden;
+  --web-view-name: Web 1920 – 1;
+  --web-view-id: Web_1920__1;
+  --web-scale-on-resize: true;
+  --web-enable-deep-linking: true;
+}
+#Component_5__1 {
+  position: absolute;
+  width: 53.189px;
+  height: 46.002px;
+  left: 9.811px;
+  top: 6.935px;
+  overflow: visible;
+}
+#Component_4__1 {
+  position: absolute;
+  width: 47px;
+  height: 40px;
+  left: 6.189px;
+  top: 2.065px;
+  overflow: visible;
+}
+#Circle_component {
+  position: absolute;
+  width: 47px;
+  height: 40px;
+  left: 0px;
+  top: 0px;
+  overflow: visible;
+}
+#Ellipse_1 {
+  fill: rgba(255, 255, 255, 1);
+  stroke: rgba(229, 28, 28, 1);
+  stroke-width: 1px;
+  stroke-linejoin: miter;
+  stroke-linecap: butt;
+  stroke-miterlimit: 4;
+  shape-rendering: auto;
+}
+.Ellipse_1 {
+  position: absolute;
+  overflow: visible;
+  width: 47px;
+  height: 40px;
+  left: 0px;
+  top: 0px;
+}
+#yo {
+  left: 13px;
+  top: 5px;
+  position: absolute;
+  overflow: visible;
+  width: 22px;
+  white-space: nowrap;
+  text-align: left;
+  font-family: Helvetica Neue;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 20px;
+  color: rgba(112, 112, 112, 1);
+}
+#Path_1 {
+  fill: transparent;
+  stroke: rgba(112, 112, 112, 1);
+  stroke-width: 1px;
+  stroke-linejoin: miter;
+  stroke-linecap: butt;
+  stroke-miterlimit: 4;
+  shape-rendering: auto;
+}
+.Path_1 {
+  overflow: visible;
+  position: absolute;
+  width: 53.922px;
+  height: 46.817px;
+  left: 0px;
+  top: 0px;
+  transform: matrix(1, 0, 0, 1, 0, 0);
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <div class="circleContainer">
+      <circle-component></circle-component>
+    </div>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+import CircleComponent from "@/components/CircleComponent.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld,
+    CircleComponent
+  }
+};
+</script>
+
+<style lang="sass">
+.circleContainer
+  margin-left: 600px
+</style>
